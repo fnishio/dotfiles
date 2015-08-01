0974a2b277f85a6066ec887ae97c022f56a866d6
Fumihiko Nishio <fumihiko.nishio+github@gmail.com>
2015-08-01 21:00:14 +0900

    Change the name of dot files.

--- mklink.sh
+++ mklink.sh
@@ -4,7 +4,7 @@ DOTFILES_DIR=$(cd $(dirname $0); pwd)
 
 function link() {
   SRC_FILE=${DOTFILES_DIR/#${HOME}/.}/$1
-  LNK_FILE=${HOME}/$1
+  LNK_FILE=${HOME}/${1/dot\./\.}
   DATE=`date +"%Y%m%d_%H%M%S"`
 
   # if a file exists, backup it
@@ -19,12 +19,6 @@ function link() {
   ln -sf ${SRC_FILE} ${LNK_FILE}
 }
 
-# find dotfiles except for current dir and .git*
-function dotfiles() {
-  find $1 -name '.*' -maxdepth 1 | \
-  egrep -v '\.$|\.git'
-}
-
-for file in `dotfiles ${DOTFILES_DIR}`; do
+for file in ${DOTFILES_DIR}/dot.*; do
   link `basename $file`
 done
